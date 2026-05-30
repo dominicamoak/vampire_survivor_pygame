9d13b8362e782e332bb36cb1abe3d9bbbee72ca2
Dominic Amoak <amoakdominic76@gmail.com>
2026-05-31 01:51:33 +0200

    Added the world map, collisions and sprite objects

--- code/main.py
+++ code/main.py
@@ -1,5 +1,8 @@
 from settings import *
-from player import *
+from player import Player
+from sprites import *
+from pytmx.util_pygame import load_pygame
+from random import randint
 
 
 class Game():
@@ -14,9 +17,22 @@ class Game():
         # Surfaces / Imports
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         
-        # Sprites
+        # Groups
         self.all_sprites = pygame.sprite.Group()
-        self.player = Player(self.all_sprites, (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.collision_sprites = pygame.sprite.Group()
+        
+        self.setup()
+        
+        # Sprites / Groups
+        self.player = Player(self.all_sprites, (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2), self.collision_sprites)
+    
+    def setup(self):
+        map = load_pygame(join('data', 'maps', 'world.tmx'))
+        for x, y, image in map.get_layer_by_name('Ground').tiles():
+            Sprite(self.all_sprites, (x * TILE_SIZE, y * TILE_SIZE), image)
+        
+        for obj in map.get_layer_by_name('Objects'):
+            CollisionSprite((self.all_sprites, self.collision_sprites), (obj.x, obj.y), obj.image)
     
     def run(self):
         while self.running:
@@ -27,7 +43,7 @@ class Game():
             self.draw()
             
         pygame.quit()
-        
+    
     # Event Loop
     def events(self):
         for event in self.game_events:
@@ -37,7 +53,7 @@ class Game():
     # Updates
     def update(self):
         self.all_sprites.update(self.dt)
-        
+    
     # Draw Game
     def draw(self):
         self.display_surface.fill('#276938')
---
+++ code/sprites.py
@@ -0,0 +1,15 @@
+from settings import *
+
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, groups, pos, surf):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(topleft = pos)
+
+class CollisionSprite(pygame.sprite.Sprite):
+    def __init__(self, groups, pos, surf):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(topleft = pos)
+
