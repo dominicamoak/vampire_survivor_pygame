8f3d7b2baa4c41bd005998c419e4476cff3756d1
Dominic Amoak <amoakdominic76@gmail.com>
2026-06-01 13:21:38 +0200

    Added the collisions boject to scene

---
+++ code/groups.py
@@ -0,0 +1,6 @@
+from settings import *
+
+class AllSprites(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+
--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
 from settings import *
 from player import Player
 from sprites import *
+from groups import AllSprites
 from pytmx.util_pygame import load_pygame
 from random import randint
 
@@ -18,7 +19,7 @@ class Game():
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         
         # Groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
         
         self.setup()
@@ -26,6 +27,7 @@ class Game():
         # Sprites / Groups
         self.player = Player(self.all_sprites, (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2), self.collision_sprites)
     
+    # Scene Setup
     def setup(self):
         map = load_pygame(join('data', 'maps', 'world.tmx'))
         for x, y, image in map.get_layer_by_name('Ground').tiles():
@@ -33,7 +35,11 @@ class Game():
         
         for obj in map.get_layer_by_name('Objects'):
             CollisionSprite((self.all_sprites, self.collision_sprites), (obj.x, obj.y), obj.image)
+        
+        for block in map.get_layer_by_name('Collisions'):
+            CollisionSprite((self.collision_sprites), (block.x, block.y), pygame.Surface((block.width, block.height)))
     
+    # Run
     def run(self):
         while self.running:
             self.game_events = pygame.event.get()
